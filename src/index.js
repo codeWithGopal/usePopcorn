e2bdef91340c675c6e542963a177a3d97badcd18
Gopal Kotwal <gkotwal@inmoment.com>
2024-01-14 21:13:45 -0500

    Design and Developed the Star and StarRating componenents using the flexbible prop for improving the reusability

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movies was ratied {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "bad", "okay", "good", "amazing"]}
+    />
+    <StarRating
+      maxRating={10}
+      size={24}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
+    <StarRating />
+    <Test />
   </React.StrictMode>
 );
